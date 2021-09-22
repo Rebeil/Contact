8a6e025d6bab4484795d6fb9fec2ddadc5480d80
Rebeil <58599895+Rebeil@users.noreply.github.com>
2021-09-23 01:39:19 +0400

    Project 1.1

--- Project.cpp
+++ Project.cpp
@@ -1,41 +1,70 @@
 #include <iostream>
-#include <string>
-#include <cassert>
-#include "Contact.h"
-#include <windows.h>
-const int two = 2;
-
-void viewing_contacts(Contact* ct) {
-    for (int i = 0; i < two; ++i) {
-        std::cout << ct[i];
-    }
-}
-
-void search_in_contacts(Contact& ct) {
-
-}
-void search_on_list_contacts() {
-
-}
-
+#include "Test.h"
+#include <Windows.h>
+int Test::count = 0;
+const int str3 = 3;
+enum Comands {
+    Add_contact = 1000,
+    Del_contac = 1001,
+    search_in_contact_name = 1002,
+    search_in_contact_last_name = 1003,
+    print_contacts = 1004
+};
 int main()
-{
+{ 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    Contact test = { "1","2","3","4","5","6" };
-    Contact* test2 = new Contact[two];
+    
+    Test *contacts = new Test[Test::getValue()]; // создаём обычные контакты
+    Test *favorites = new Test[Test::getValue()]; // создаём избранные
+    if (contacts && favorites) {
+        char buf[str3];
+        for (int i = 0; i < str3 ; ++i) {
+            buf[i] = '\0';
+        }
 
-    test2[0] = { "1","2","3","4","5","6" };
-    test2[1] = { "11","22","33","44","55","66" };
-    
-    viewing_contacts(test2);
-    Contact* a = new Contact[two];
-    a = a->deep_copy_contact(test2,two);
-    std::cout << std::endl;
-    viewing_contacts(a);
-    a = a->contact_add(a, 2);
-    viewing_contacts(a);
-    
+        int answer = 0;
+        char key;
+        while(1) {
+            std::cout << "Введите команду\n q - для выхода,\n" <<
+                " d - для удаления контакта,\n a - для добавления контакта,\n" <<
+                " p - для просмотра контактов, \n s - для поиска контакта :" << std::endl;
+            std::cin >> key;
+            std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');	//очистка буфера
+            if (key != 'q') {
+                switch (key)
+                {
+                case 's':
+                     for (int i = 0; i < (str3 - 1); ++i) std::cin >> buf[i];
+                     buf[str3 - 1] = '\0';
+                     answer = contacts->search_in_contact_name(contacts, buf);          
+                    break;
+                case 'd':
+                    contacts = contacts->contact_del(contacts);
+                    break;
+                case 'a':
+                    contacts = contacts->contact_add(contacts);
+                    break;
+                case 'p':
+                    contacts->my_print(contacts, favorites);
+                    break;
+                case 'f':
+                    favorites = favorites->contact_add(favorites);
+                    break;
+                default:
+                    std::cout << "Неверно введена команда" << std::endl;
+                    //std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');	//очистка буфера
+                    break;
+                }
+            }
+            else {
+                std::cout << "Выход из программы"; 
+                break; 
+            }
+        }
+    }
+    else {
+        std::cout << "Указатель/ли пустой/е" << std::endl;
+    }
     return 0;
 }
-
